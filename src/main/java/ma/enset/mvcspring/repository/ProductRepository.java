b430b3c6a18c839b616eaf9522f450b4cd90630a
Badr <badham048@gmail.com>
2025-04-16 20:03:09 +0200

    V2

--- src/main/java/ma/enset/mvcspring/repository/ProductRepository.java
+++ src/main/java/ma/enset/mvcspring/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package ma.enset.mvcspring.repository;
 
 import ma.enset.mvcspring.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByNameContainsIgnoreCase(String keyword, Pageable pageable);
+
 }
--- src/main/java/ma/enset/mvcspring/web/ProductController.java
+++ src/main/java/ma/enset/mvcspring/web/ProductController.java
@@ -1,8 +1,12 @@
 package ma.enset.mvcspring.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
 import ma.enset.mvcspring.entities.Product;
 import ma.enset.mvcspring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +15,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class ProductController {
-    @Autowired
+
+
     private ProductRepository productRepository;
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Product> products = productRepository.findAll();
-        model.addAttribute("listProducts", products);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "3") int s,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+
+        Page<Product> products = productRepository
+                .findByNameContainsIgnoreCase(keyword, PageRequest.of(p, s));
+
+        model.addAttribute("listProducts", products.getContent());
+        model.addAttribute("page", new int[products.getTotalPages()]);
+        model.addAttribute("currentPage", p);
+        model.addAttribute("keyword", keyword); // Pour garder la valeur du champ
         return "products";
     }
+
+
+
     @GetMapping("/delete")
     public String delete(@RequestParam(name = "id") Long id) {
         productRepository.deleteById(id);
